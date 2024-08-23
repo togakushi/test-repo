860287fef00b749eb1c56815b2cc4c127c16cfd1
togakushi <nina.togakushi+git@gmail.com>
2024-08-23 16:24:36 +0900

    Update a.py

--- a.py
+++ a.py
@@ -1,13 +1,24 @@
-#!/usr/env python
+#!/usr/bin/env python3
 
-md = '''
-## test page
+import pandas as pd
 
-| a   | b   | c   |
-| --- | --- | --- |
-| a   | b   | c   |
-| a   | b   | c   |
-| a   | b   | c   |
+df = pd.read_csv("a.csv", index_col=0)
+df2 =  pd.DataFrame(index=df.index.to_list() , columns=df.index.to_list())
+df3 =  pd.DataFrame(index=df.index.to_list() , columns=df.index.to_list())
+print(df)
+print("-" * 20)
 
-'''
-print(md)
+for idx1 in range(len(df)):
+        p1 = df.iloc[idx1]
+        for idx2 in range(len(df)):
+                p2 = df.iloc[idx2]
+
+                if p1.name == p2.name:
+                        continue
+                df2[p2.name][p1.name] = (p1 < p2).sum()
+                #df3[p2.name][p1.name] = p1.count()
+
+
+print("-" * 20)
+print(df2)
+print(df3)
