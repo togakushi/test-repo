f3424ee25cd84aba0a03fe34b9b5c22e001e71fd
togakushi <nina.togakushi+git@gmail.com>
2024-08-23 17:57:29 +0900

    Update a.py

--- a.py
+++ a.py
@@ -3,22 +3,35 @@
 import pandas as pd
 
 df = pd.read_csv("a.csv", index_col=0)
-df2 =  pd.DataFrame(index=df.index.to_list() , columns=df.index.to_list())
-df3 =  pd.DataFrame(index=df.index.to_list() , columns=df.index.to_list())
+df2 =  pd.DataFrame(index=df.index.to_list() , columns=df.index.to_list() + ["total"])
 print(df)
 print("-" * 20)
 
 for idx1 in range(len(df)):
         p1 = df.iloc[idx1]
+        t_c = 0
+        t_w = 0
         for idx2 in range(len(df)):
                 p2 = df.iloc[idx2]
-
                 if p1.name == p2.name:
+                        df2[p2.name][p1.name] = ""
                         continue
-                df2[p2.name][p1.name] = (p1 < p2).sum()
-                df3[p2.name][p1.name] = len(pd.concat([p1,p2], axis=1).dropna())
 
+                t = len(pd.concat([p1,p2], axis=1).dropna())
+                w = (p1 < p2).sum()
+                l = t - w
 
-print("-" * 20)
-print(df2)
-print(df3)
+                t_c += t
+                t_w += w
+
+                if t:
+                        p = round(float(w / t * 100), 1)
+                else:
+                        p = "--.-"
+                df2[p2.name][p1.name] = f"{w}-{l} ({p}%)"
+
+        if t_c:
+                t_p = round(float(t_w / t_c * 100), 1)
+        else:
+                t_p = "--.-"
+        df2["total"][p1.name] = f"{t_w}-{t_c-t_w} ({t_p}%)"
